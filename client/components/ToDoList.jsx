87d893fe6bc35e9a10b2e6729c68fb8fcc706a57
albasychris <albasychris@gmail.com>
2020-11-08 10:33:05 +1300

    use apollo and graphql to display a list of lists

--- client/components/ToDoList.jsx
+++ client/components/ToDoList.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import { useQuery, gql, useMutation } from '@apollo/client';
+import { connect } from 'react-redux'
 const todolist = (props) => {
+
+  const GET_LISTS = gql`
+  query {
+    lists (user_id: ${props.userId} ) {
+      name
+    }
+  }
+`;
+  const lists = useQuery(GET_LISTS)
+  console.log(lists);
   return (
-    <h1>Welcome to your to do list</h1>
+    <>
+      <h1>Welcome to your to do list</h1>
+      {lists.data !== undefined && lists.data.lists.map(list => <div>{list.name}</div>)}
+    </>
   )
 }
 
-export default todolist
+const mapStateToProps = (state) => {
+  return {
+    userId: state.auth.user.id
+  }
+}
+
+export default connect(mapStateToProps)(todolist)

4cf71d54de536d1c2dff63b91dc536187c1c04b6
malyz <mattuy001@gmail.com>
2020-11-11 09:01:48 +1300

    refactor code

--- client/components/ToDoList.jsx
+++ client/components/ToDoList.jsx
@@ -3,69 +3,36 @@ import { useQuery, gql, useMutation } from '@apollo/client'
 import { connect } from 'react-redux'
 import Button from 'react-bootstrap/Button'
 
-const GET_ITEMS = gql`
-  query Items($todolist_ID: ID!) {
-    items(todolist_id: $todolist_ID) {
-      item
-      id
-    }
-  }
-`
-
 const ADD_ITEM = gql`
   mutation AddItem($item: String!, $todolist_id: ID!) {
     addItem(item: $item, todolist_id: $todolist_id) {
       id
-      item
-      todolist_id
+      name
+      user_id
+      items {
+        item
+      }
     }
   }
 `
 const Todolist = (props) => {
+  const { id, items } = props.list
   const [item, setItem] = useState('')
-  console.log(props)
-  const [addItem] = useMutation(ADD_ITEM, {
-    update: (cache, data) => {
-      // cache.modify({
-      //   fields: {
-      //     items(existingItems = []) {
-      //       const newItemRef = cache.writeFragment({
-      //         data: addItem,
-      //         fragment: gql`
-      //           fragment NewItem on Item {
-      //             id
-      //             item
-      //             todolist_id
-      //           }
-      //         `
-      //       })
-      //       console.log(existingItems)
-      //       return [...existingItems, newItemRef]
-      //     }
-      //   }
-      // })
-    }
-  })
-
-  const items = useQuery(GET_ITEMS, {
-    variables: { todolist_ID: props.todolistid }
-  })
+  const [addItem] = useMutation(ADD_ITEM)
 
   const handleAddItem = () => {
     addItem({
-      variables: { item: item, todolist_id: props.todolistid }
+      variables: { item: item, todolist_id: id }
     })
   }
   const handleChange = (evt) => {
     setItem(evt.target.value)
   }
 
-  if (items.loading) return <div>loading...</div>
   return (
     <div style={{ background: 'black', color: 'white' }}>
       <h3>List Items</h3>
-      {items.data &&
-        items.data.items.map((item, i) => <div key={i}>{item.item}</div>)}
+      {items && items.map((el, i) => <div key={i}>{el.item}</div>)}
       <input
         onChange={handleChange}
         type="text"

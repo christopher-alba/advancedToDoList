0fc34bd285164a09e7ddb02316ad6d6e24871309
malyz <mattuy001@gmail.com>
2020-11-10 20:22:52 +1300

    implemented uuid

--- seeds/2-users.js
+++ seeds/2-users.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcrypt')
+const { v4 } = require()
 
-exports.seed = async function(knex) {
-  let password = await bcrypt.hash('123456',10)
+exports.seed = async function (knex) {
+  let password = await bcrypt.hash('123456', 10)
   // Inserts seed entries
   return knex('users').insert([
-    {id: 1, email:'proteus@gmail.com', password: password},
-  ]);
-  
-};
+    { id: v4(), email: 'proteus@gmail.com', password: password }
+  ])
+}

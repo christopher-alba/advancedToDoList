e5419bd03f9aae01a3c1cadbf91c4a67f34fb720
malyz <mattuy001@gmail.com>
2020-11-10 20:21:24 +1300

    fix in progress for mutation

--- client/components/ToDoList.jsx
+++ client/components/ToDoList.jsx
@@ -2,32 +2,18 @@ import React, { useState } from 'react'
 import { useQuery, gql, useMutation } from '@apollo/client'
 import { connect } from 'react-redux'
 import Button from 'react-bootstrap/Button'
-import UsersLists from './UsersLists'
 
-
-<<<<<<< HEAD
 const GET_ITEMS = gql`
-  query Items($todolist_ID: Int!) {
-    items(todolist_ID: $todolist_ID) {
+  query Items($todolist_ID: ID!) {
+    items(todolist_id: $todolist_ID) {
       item
       id
     }
   }
 `
 
-=======
-
-const GET_ITEMS = gql`
-query Items($todolist_ID: Int! ){
-  items (todolist_ID: $todolist_ID ) {
-    item
-    id
-  }
-}
-`;
->>>>>>> 62db1e5c468118f5520ceb341ea15d218d13b6eb
 const ADD_ITEM = gql`
-  mutation AddItem($item: String!, $todolist_id: Int!) {
+  mutation AddItem($item: String!, $todolist_id: ID!) {
     addItem(item: $item, todolist_id: $todolist_id) {
       id
       item
@@ -35,66 +21,35 @@ const ADD_ITEM = gql`
     }
   }
 `
-const todolist = (props) => {
+const Todolist = (props) => {
   const [item, setItem] = useState('')
-
+  console.log(props)
   const [addItem] = useMutation(ADD_ITEM, {
-    update(cache, { data: { addItem } }) {
-      cache.modify({
-        fields: {
-          items(existingItems = []) {
-            const newItemRef = cache.writeFragment({
-              data: addItem,
-              fragment: gql`
-                fragment NewItem on Item {
-                  id
-                  type
-                }
-              `
-            })
-            return [...existingItems, newItemRef]
-          }
-        }
-      })
+    update: (cache, data) => {
+      // cache.modify({
+      //   fields: {
+      //     items(existingItems = []) {
+      //       const newItemRef = cache.writeFragment({
+      //         data: addItem,
+      //         fragment: gql`
+      //           fragment NewItem on Item {
+      //             id
+      //             item
+      //             todolist_id
+      //           }
+      //         `
+      //       })
+      //       console.log(existingItems)
+      //       return [...existingItems, newItemRef]
+      //     }
+      //   }
+      // })
     }
   })
 
-<<<<<<< HEAD
-  const items = useQuery(GET_ITEMS, { variables: { todolist_ID: todolistid } })
-=======
-  const [item, setItem] = useState("")
-
-  const [addItem] = useMutation(ADD_ITEM, {
-    update(cache, { data: { addItem } }) {
-      console.log(cache);
-      cache.modify({
-        fields: {
-          item(existingItems = []) {
-            const newItemRef = cache.writeFragment({
-              data: addItem,
-              fragment: gql`
-                fragment NewItem on Item {
-                  id
-                  item
-                  todolist_id
-                }
-              `
-             
-            });
-            console.log(existingItems);
-            return [...existingItems, newItemRef];
-          }
-        }
-      });
-    }
-  });
-
-  
-  let items = useQuery(GET_ITEMS, { variables: { todolist_ID: props.todolistid } })
-
-
-
->>>>>>> 62db1e5c468118f5520ceb341ea15d218d13b6eb
+  const items = useQuery(GET_ITEMS, {
+    variables: { todolist_ID: props.todolistid }
+  })
 
   const handleAddItem = () => {
     addItem({
@@ -104,28 +59,20 @@ const todolist = (props) => {
   const handleChange = (evt) => {
     setItem(evt.target.value)
   }
+
+  if (items.loading) return <div>loading...</div>
   return (
-    <>
-<<<<<<< HEAD
-      <h1>Welcome to your to do list</h1>
-      <h3>Lists</h3>
-      {lists.data !== undefined &&
-        lists.data.lists.map((list) => (
-          <div onClick={() => handleListClick(list.id)}>{list.name}</div>
-        ))}
-=======
->>>>>>> 62db1e5c468118f5520ceb341ea15d218d13b6eb
-      <div style={{ background: 'black', color: 'white' }}>
-        <h3>List Items</h3>
-        {items.data && items.data.items.map((item) => <div>{item.item}</div>)}
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="add another item"
-        />
-        <Button onClick={() => handleAddItem()}>+</Button>
-      </div>
-    </>
+    <div style={{ background: 'black', color: 'white' }}>
+      <h3>List Items</h3>
+      {items.data &&
+        items.data.items.map((item, i) => <div key={i}>{item.item}</div>)}
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="add another item"
+      />
+      <Button onClick={() => handleAddItem()}>+</Button>
+    </div>
   )
 }
 
@@ -135,4 +82,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(todolist)
+export default connect(mapStateToProps)(Todolist)

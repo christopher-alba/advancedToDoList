6a081c14cee6696cd4a982dc4ebace1528d473a3
malyz <mattuy001@gmail.com>
2020-11-10 20:22:31 +1300

    fix spacing

--- migrations/20201107140632_todolistItems.js
+++ migrations/20201107140632_todolistItems.js
@@ -1,12 +1,11 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('todolistItems', (t) => {
     t.increments('id').primary()
     t.integer('todolist_id').references('todolists.id')
     t.string('item')
   })
-};
+}
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('todolistItems')
-};
+}

8ff0e409ab39b66a3eebe3e39467c1d2c3cffe17
malyz <mattuy001@gmail.com>
2020-11-10 20:21:51 +1300

    refactor code

--- client/components/UsersLists.jsx
+++ client/components/UsersLists.jsx
@@ -1,35 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useQuery, gql } from '@apollo/client'
 import ToDoList from './ToDoList'
 import { connect } from 'react-redux'
+
 const GET_LISTS = gql`
-  query Lists($user_id: Int! ) {
-    lists (user_id: $user_id) {
-      name
+  query Lists($user_id: ID!) {
+    lists(user_id: $user_id) {
       id
+      name
+      user_id
     }
   }
 `
 
 const UsersLists = (props) => {
   const [todolistid, settodolistid] = useState('')
-  let lists = useQuery(GET_LISTS, { variables: { user_id: props.userId } })
-
-
+  let { data, loading } = useQuery(GET_LISTS, {
+    variables: { user_id: props.userId }
+  })
 
   const handleListClick = (id) => {
     settodolistid(id)
   }
 
-  if (lists.loading) {
+  if (loading) {
     return <div>Loading...</div>
   }
-
+  console.log(data)
   return (
     <>
       <h1>Welcome to your to do list</h1>
       <h3>Lists</h3>
-      { lists.data !== undefined && lists.data.lists.map(list => <div onClick={() => handleListClick(list.id)}>{list.name}</div>)}
+      {data &&
+        data.lists.map((list, i) => (
+          <div key={i} onClick={() => handleListClick(list.id)}>
+            {list.name}
+          </div>
+        ))}
       {todolistid && <ToDoList todolistid={todolistid} />}
     </>
   )

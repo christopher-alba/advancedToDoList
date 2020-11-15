24a0f2070debe20146d08bf9e20aa2dd8a8bdd9d
albasychris <albasychris@gmail.com>
2020-11-16 10:32:52 +1300

    remove unique constraint for list name

--- migrations/20201107140340_todolists.js
+++ migrations/20201107140340_todolists.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('todolists', (t) => {
     t.increments('id').primary()
     t.integer('user_id').references('users.id')
-    t.string('name').unique()
+    t.string('name')
   })
 }
 
